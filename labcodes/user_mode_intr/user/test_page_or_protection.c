aad1aa8fe9f2d7ebad2ef82059ad4505cd3a550a
MashPlant <740678788@qq.com>
2019-06-27 07:12:16 +0800

    qs

--- labcodes/user_mode_intr/user/test_page_or_protection.c
+++ labcodes/user_mode_intr/user/test_page_or_protection.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main() {
-  *(int *)(0xc01043c0) = 0; // the address of page fault handler
+  // *(int *)(0xc01043c0) = 0; // the address of page fault handler
+  // int a = *(int *)(0xc0158028);
   // will not get a general protection fault
   // instead will get a page fault, with err code = 7
   cprintf("I survived\n"); // of course will not survive
